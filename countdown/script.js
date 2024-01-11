70b65b39fb928ae89573eada92c43d7ed1635403
yoonhee Lee <105180536+yoonhee032@users.noreply.github.com>
2024-01-11 21:20:44 +0900

    desc : 타이머 입력값을 숫자로 제한

--- countdown/script.js
+++ countdown/script.js
@@ -8,6 +8,7 @@ const reset_btn = document.getElementById("reset_btn");
 
 //타이머의 상태를 저장 (실행전, 실행중, 실행완료)
 const disp = document.getElementById("disp");
+const numbers = document.querySelector(".number-container");
 
 let interval = null;
 let total = 0;
@@ -15,7 +16,9 @@ let total = 0;
 //유저가 입력한 총 시간을 total에 저장
 const totalValue = () => {
   total =
-    Number(hour.value) * 3600 + Number(minute.value) * 60 + Number(second.value);
+    Number(hour.value) * 3600 +
+    Number(minute.value) * 60 +
+    Number(second.value);
 };
 
 //줄어드는 시간을 처리
@@ -24,20 +27,25 @@ const timer = () => {
   total--;
 
   if (total >= 0) {
-    let hr = Math.floor(total / 3600);              
+    let hr = Math.floor(total / 3600);
     let mn = Math.floor((total % 3600) / 60);
     let sc = total % 60;
 
-    //계산된 시간을 string으로 변경 
-    hour.value = String(hr).padStart(2, "0");       
+    //계산된 시간을 string으로 변경
+    hour.value = String(hr).padStart(2, "0");
     minute.value = String(mn).padStart(2, "0");
     second.value = String(sc).padStart(2, "0");
-
   } else {
     disp.innerText = "Time Over !!";
   }
 };
 
+//입력값을 숫자로 제한
+numbers.addEventListener("input", (e) => {
+  e.target.value = e.target.value
+    .replace(/[^0-9.]/g, "")
+    .replace(/(\..*)\./g, "$1");
+});
 
 //start 버튼을 누르면 실행
 start_btn.addEventListener("click", () => {
@@ -45,7 +53,6 @@ start_btn.addEventListener("click", () => {
 
   //1초마다 timer() 함수를 실행시킨다.
   interval = setInterval(timer, 1000);
-
   disp.innerText = "Timer started";
 });
 
